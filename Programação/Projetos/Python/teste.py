f99096d7989b01a87ea251c1f9e632bf846038de
Marcos Raulisson <marcosraulisson@gmail.com>
2023-12-20 15:16:46 -0300

    scripts de estudo e análise

--- Programação/Projetos/Python/teste.py
+++ Programação/Projetos/Python/teste.py
@@ -1,8 +1,36 @@
-chama = None
+from getpass import getpass
+import os
+
+banco = {}
 
 
-def funcao(opcao):
-    opcao = input('Chora mais')
+def cadastro(banco):
+
+    banco['new_user'] = input('Digite um novo usuário: ')
+    banco['new_password'] = input('Digite uma nova senha: ')
+
+    return banco
 
 
-funcao(chama)
+def login():
+    print('Insira seu usuário e senha.')
+
+    banco['user'] = input('Digite seu usuário: ')
+    banco['password'] = input('Digite sua senha: ')
+
+    return banco
+
+
+if banco['user'].keys() == banco['new_user'].keys():
+    print('Você entrou')
+
+print(banco)
+
+# print('Seja bem vindo ao Banco Central')
+# i = input('Tecle:\n'
+#           '[E] - Entrar\n'
+#           '[C] - Cadastrar\n'
+#           '[S] - Sair').upper()
+
+# if i == 'E':
+#     return login()
